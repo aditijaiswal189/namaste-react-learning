9488d25cff4b0400c00f600e4164dd276cf7fa8e
aditijaiswal189 <aditijaiswal189@gmail.com>
2024-02-06 16:28:13 +0530

    initial commit
    initial commit-episode1

---
+++ App.js
@@ -0,0 +1,14 @@
+const parent = React.createElement("div", { id: 'parent' }, [
+  React.createElement("div", { id: 'child1' },
+    [React.createElement("h1", { id: "heading" }, "Hello, world from React! I am h1"),
+    React.createElement("h2", { id: "heading" }, "Hello, world from React! I am h2")]
+  ), React.createElement("div", { id: 'child2' },
+    [React.createElement("h1", { id: "heading" }, "Hello, world from React! I am h1"),
+    React.createElement("h2", { id: "heading" }, "Hello, world from React! I am h2")]
+  )
+]);
+
+console.log(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
